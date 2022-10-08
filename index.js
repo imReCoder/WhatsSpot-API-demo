709ef6940ddc84db9e8ef7b7c0d8883d9a2550d6
Ranjit Kumar Pandit <ranjitkumar448@yahoo.com>
2022-10-08 18:53:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const axios = require("axios");
 const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ3YWxsZXRJZCI6IjYyNmZhMzgzNmE2MmZiMmVmZDRlODEwMSIsInVzZXJJZCI6IjYyNmZhMzgzNmE2MmZiMmVmZDRlODEwMiIsImRldmljZUlkIjoiNjI2ZmEzYjA2YTYyZmIyZWZkNGU4MTAzIiwiaWF0IjoxNjUyMDcyNzQxfQ.tH6o8YOF2EV3tLy19OnG4KjsGLH-6CA_b2YKOcMZsqs";
-const whatsSpotURL = "https://backend.whatsspot.in";
+const whatsSpotURL = "https://api.whatsspot.in/api";
 
 const sendTextMessage = async()=>{
   let body = {
-    numbers:"7984545163", //Ex: 9878976545,9894534567,9234387658,
+    numbers:"+911234567890", //Ex: +909878976545,+429894534567,9234387658,
     message:{
       text:"Hey sending message using WhatsSpot API"
     }
